fbf69e29f1928ad7af0b51cf6f26acc469367a1c
rslowinski <rslowinski97@gmail.com>
2024-02-14 17:00:00 +0100

    respond with pong

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_stream(mut tcp_stream: TcpStream) {
     let mut input_buf = [0; 512];
-    let mut output_buf = b"+PONG\r\n";
+    let output_buf = b"+PONG\r\n";
     loop {
-        // tcp_stream.read(&mut input_buf).expect("Failed to read from client");
-        tcp_stream.write_all(&mut output_buf).expect("Failed to wrtie to client");
+        tcp_stream.read(&mut input_buf).expect("Failed to read from client");
+        tcp_stream.write_all(output_buf).expect("Failed to wrtie to client");
     }
 
 }
