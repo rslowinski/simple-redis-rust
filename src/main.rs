ac77dbea4659d34c546d480e1ebee04737fbf102
rslowinski <rslowinski97@gmail.com>
2024-02-14 10:38:43 +0100

    respond with pong

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+
+fn handle_stream(mut tcp_stream: TcpStream) {
+    let mut input_buf = [0; 512];
+    let mut output_buf = b"+PONG\r\n";
+    loop {
+        tcp_stream.read(&mut input_buf).expect("Failed to read from client");
+        tcp_stream.write_all(&mut output_buf).expect("Failed to wrtie to client");
+    }
+
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +23,8 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                handle_stream(_stream)
             }
             Err(e) => {
                 println!("error: {}", e);
