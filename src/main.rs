905f37025557e0f0af5f7b87d68d67cc0c927eba
rslowinski <rslowinski97@gmail.com>
2024-02-15 08:55:49 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{Read, Write};
-// Uncomment this block to pass the first stage
 use std::net::{TcpListener, TcpStream};
 
 
@@ -14,13 +13,8 @@ fn handle_stream(mut tcp_stream: TcpStream) {
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
